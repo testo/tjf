4642b63569eb11ec278b7216c247aacbd0173f47
Bernardo Abreu Figueiredo <babreufigueiredo@testo.de>
2020-06-23 08:11:11 +0200

    added swagger definition, and script to generate html docs

---
+++ create_docs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+if [ ! -f "openapi-generator-cli.jar" ]; then
+wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/4.3.1/openapi-generator-cli-4.3.1.jar -O openapi-generator-cli.jar
+fi
+java -jar openapi-generator-cli.jar generate \
+  -i api/generalDeviceInfo.yaml \
+  -g html \
+  -o docs/
