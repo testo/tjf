aee4ca1af6c2436763d0cd79859590ed784ce240
Bernardo Abreu Figueiredo <babreufigueiredo@testo.de>
2020-06-25 11:04:22 +0200

    changed unittests to pytest

--- tests/legacyschematest.py
+++ tests/legacyschematest.py
@@ -3,21 +3,22 @@ import requests
 from jsonschema import validate
 import json
 import os
+import pytest
 
-class TestRequests(unittest.TestCase):
-    #Before execution, a Flask server needs to be started
-    url = 'http://localhost:5000'
+#Before execution, a Flask server needs to be started
+url = 'http://localhost:5000'
 
-    def test_matchSchema(self):
-        response = requests.get(url=self.url + '/deviceInfo')
-        self.assertEquals(200, response.status_code)
-        with open(os.path.dirname(__file__) + '/../schema/v2.0.0/deviceInfo-schema-legacy.json') as json_file:
-            schema = json.load(json_file)
-            instance = response.json()
-            try:
-                validate(instance=instance, schema=schema)
-            except:
-                self.fail("Validation failed!")
+def test_matchSchema():
+    response = requests.get(url=url + '/deviceInfo')
+    assert response.status_code == 200
+    with open(os.path.dirname(__file__) + '/../schema/v2.0.0/deviceInfo-schema-legacy.json') as json_file:
+        schema = json.load(json_file)
+        instance = response.json()
+        try:
+            validate(instance=instance, schema=schema)
+        except:
+            pytest.fail('Validation failed!')
 
-if __name__ == '__main__':
-    unittest.main()
+def test_postFail():
+    response = requests.post(url=url + '/deviceInfo')
+    assert response.status_code != 200
